dfc163f73fa347803289452c42613415fa9ae27d
hejingar <hejingoh@gmail.com>
2023-11-12 10:17:05 +0100

    feature:select email extension

--- contentScript.js
+++ contentScript.js
@@ -1,53 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var scrapeTabButton = document.getElementById('scrapeTabButton');
-    var settingsTabButton = document.getElementById('settingsTabButton');
-
-    scrapeTabButton.addEventListener('click', function() {
-        openTab('scrapeTab');
-    });
-
-    settingsTabButton.addEventListener('click', function() {
-        openTab('settingsTab');
-    });
-
-
-    openTab('scrapeTab');
-});
-
 // Listen for a message from the popup
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.action === "scrapeEmails") {
-            const scrapedEmails = scrapeEmails();
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "scrapeEmails") {
+        scrapeEmails().then(scrapedEmails => {
+            console.log("in listener Scraped emails:", scrapedEmails);
             if (scrapedEmails.length > 0) {
                 chrome.runtime.sendMessage({emails: scrapedEmails});
             } else {
                 chrome.runtime.sendMessage({message: "No emails found"});
             }
-        }
+        });
     }
-);
+});
 
-// Function to scrape emails from the webpage
+
+// Scrapuu
 function scrapeEmails() {
-    const emailRegex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}/g;
-    const pageText = document.body.innerText;
-    const emails = pageText.match(emailRegex) || [];
-    return [...new Set(emails)];
+    return new Promise((resolve, reject) => {
+        // local storage secure af async so need promise 
+        chrome.storage.local.get("emailType", function(data) {
+            var emailType = data.emailType || 'all';
+            var emailRegex;
+            // in case you want a specific email extension, modify the code here
+            // :/[a-zA-Z0-9._-]+@gmail\.[a-zA-Z]{2,6}/g
+            // :/[a-zA-Z0-9._-]+@gmail\.com/g
+            switch (emailType) {
+                case 'gmail':
+                    emailRegex = /[a-zA-Z0-9._-]+@gmail\.[a-zA-Z]{2,6}/g;
+                    break;
+                case 'outlook':
+                    emailRegex = /[a-zA-Z0-9._-]+@outlook\.[a-zA-Z]{2,6}/g;
+                    break;
+                case 'hotmail':
+                    emailRegex = /[a-zA-Z0-9._-]+@hotmail\.[a-zA-Z]{2,6}/g;
+                    break;
+                default:
+                    emailRegex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}/g;
+                    break;
+            }
+    
+            const pageText = document.body.innerText;
+            const emails = pageText.match(emailRegex) || [];
+            console.log("in function Scraped emails:", emails);
+            resolve(emails);
+        });
+    });
 }
 
-function openTab(tabId) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-        tabcontent[i].classList.remove('active-tab');
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    var activeTab = document.getElementById(tabId);
-    activeTab.style.display = "block";
-    activeTab.classList.add('active-tab');
-}
--- popup.js
+++ popup.js
@@ -1,16 +1,49 @@
-document.getElementById('scrapeButton').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "scrapeEmails"});
+// wait for dom to load to attach events
+document.addEventListener('DOMContentLoaded', function() {
+    var scrapeTabButton = document.getElementById('scrapeTabButton');
+    var settingsTabButton = document.getElementById('settingsTabButton');
+
+    scrapeTabButton.addEventListener('click', function() {
+        openTab('scrapeTab');
     });
+
+    settingsTabButton.addEventListener('click', function() {
+        openTab('settingsTab');
+    });
+
+    //contentScript listener
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+        const emailList = document.getElementById('emailList');
+        console.log("Message received in popup:", message);
+        if (message.emails && message.emails.length > 0) {
+            emailList.textContent = message.emails.join(', ');
+        } else if (message.message) {
+            emailList.textContent = message.message;
+        }
+    });
+
+    document.getElementById('scrapeButton').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "scrapeEmails"});
+        });
+    });
+
+    document.getElementById('emailType').addEventListener('change', function() {
+        var emailType = this.value;
+        chrome.storage.local.set({ "emailType": emailType }, function() {
+            console.log('Email type is set to ' + emailType);
+        });
+    });
+
+    openTab('scrapeTab');
 });
 
-// Listen for messages from the content script
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    const emailList = document.getElementById('emailList');
-
-    if(message.emails) {
-        emailList.textContent = message.emails.join(', ');
-    } else if (message.message) {
-        emailList.textContent = message.message;
+// Function to open a specific tab
+function openTab(tabId) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-});
+    document.getElementById(tabId).style.display = "block";
+}
